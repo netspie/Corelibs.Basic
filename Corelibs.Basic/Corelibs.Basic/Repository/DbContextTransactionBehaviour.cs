94b465fef19761081faec1dacc15716a4a6e899b
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-12-25 18:58:20 +0100

    Add db context transaction mediator pipeline behaviour

---
+++ Corelibs.Basic/Corelibs.Basic/Repository/DbContextTransactionBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Corelibs.Basic.Repository
+{
+    public class DbContextTransactionBehaviour<TCommand, TResponse> : IPipelineBehavior<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+    {
+        private readonly DbContext _context;
+
+        public DbContextTransactionBehaviour(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask<TResponse> Handle(TCommand command, CancellationToken ct, MessageHandlerDelegate<TCommand, TResponse> next)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                var response = await next(command, ct);
+
+                await transaction.CommitAsync();
+
+                return response;
+            }
+        }
+    }
+}
