aa553a27155b96b02857888ef8f24527be017d91
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-02 13:56:46 +0100

    Add guid extensions

---
+++ Corelibs.Basic/Corelibs.Basic/Collections/GuidExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Common.Basic.Collections;
+
+namespace Corelibs.Basic.Collections
+{
+    public static class GuidExtensions
+    {
+        public static bool IsIDs(this IEnumerable<string> ids)
+        {
+            if (ids.IsNullOrEmpty())
+                return false;
+
+            return ids.All(IsID);
+        }
+
+        public static bool IsID(this string id)
+        {
+            if (id.IsNullOrEmpty())
+                return false;
+
+            return Guid.TryParse(id, out var guid);
+        }
+    }
+}
