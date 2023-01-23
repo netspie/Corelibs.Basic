2bdd3ea12c62cb79fb9cb0dc8f6ee2b6a6d67ef0
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-23 18:51:42 +0100

    Add get attribute extension

---
+++ Corelibs.Basic/Corelibs.Basic/Reflection/TypeGetAttributeExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Corelibs.Basic.Reflection
+{
+    public static class TypeGetAttributeExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum value)
+            where TAttribute : Attribute
+        {
+            var type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name == null)
+                return null;
+
+            var field = type.GetField(name);
+            if (field == null)
+                return null;
+
+            var attr = Attribute.GetCustomAttribute(field, typeof(TAttribute)) as TAttribute;
+            if (attr == null)
+                return null;
+
+            return attr;
+        }
+    }
+}
