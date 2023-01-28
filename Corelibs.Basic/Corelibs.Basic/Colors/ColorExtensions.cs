79d03d9cde001d1cf776df2702f99dbb396c7834
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-28 02:39:13 +0100

    Add color extensions

---
+++ Corelibs.Basic/Corelibs.Basic/Colors/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Corelibs.Basic.Colors
+{
+    public static class ColorExtensions
+    {
+        public static string ToHexString(this Color color) =>
+            "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+
+        public static string ToRgbaString(this Color color) =>
+            $"rgba({color.R}, {color.G}, {color.B}, {color.A / 255})";
+    }
+}
