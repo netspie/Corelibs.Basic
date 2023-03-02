efa98b1ac4bcef10293044c9734d355d0788633e
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-03-02 19:43:53 +0100

    Add an interface for a search engine

---
+++ Corelibs.Basic/Corelibs.Basic/Searching/ISearchEngine.cs
@@ -0,0 +1,11 @@
+﻿namespace Corelibs.Basic.Searching
+{
+    public interface ISearchEngine<T>
+    {
+        void Add(SearchIndexData data);
+        void Add(IEnumerable<SearchIndexData> data);
+        SearchIndexData[] Search(string searchTerm);
+    }
+
+    public record SearchIndexData(string ID, string Name);
+}
