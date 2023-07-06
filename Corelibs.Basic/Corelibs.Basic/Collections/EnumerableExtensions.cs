bf69f1004817072cf08e221a8f86a8803f281a53
drlsn <dariusz.lusnia@gmail.com>
2023-07-06 13:05:24 +0200

    Add extension

--- Corelibs.Basic/Corelibs.Basic/Collections/EnumerableExtensions.cs
+++ Corelibs.Basic/Corelibs.Basic/Collections/EnumerableExtensions.cs
@@ -250,5 +250,23 @@ namespace Corelibs.Basic.Collections
 
         public static bool Contains<T>(this IEnumerable<object> source) =>
             source.OfType<T>().Any();
+
+        public static bool HasSameElements<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
+        {
+            var cnt = new Dictionary<T, int>();
+            foreach (T s in list1)
+                if (cnt.ContainsKey(s))
+                    cnt[s]++;
+                else
+                    cnt.Add(s, 1);
+
+            foreach (T s in list2)
+                if (cnt.ContainsKey(s))
+                    cnt[s]--;
+                else
+                    return false;
+            
+            return cnt.Values.All(c => c == 0);
+        }
     }
 }
