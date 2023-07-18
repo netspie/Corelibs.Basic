f1bbe0d28a4c466eda23959a8df6cfa1b7e29b69
drlsn <dariusz.lusnia@gmail.com>
2023-07-18 22:25:53 +0200

    Change EntityId from record to class

--- Corelibs.Basic/Corelibs.Basic/DDD/EntityId.cs
+++ Corelibs.Basic/Corelibs.Basic/DDD/EntityId.cs
@@ -1,8 +1,27 @@
 ﻿namespace Corelibs.Basic.DDD;
 
-public abstract record EntityId(string Value)
+public abstract class EntityId
 {
+    public string Value { get; }
+
+    public EntityId(string value) => Value = value;
+
     public static T New<T>() => (T) Activator.CreateInstance(typeof(T), Guid.NewGuid().ToString())!;
     public bool IsValid() => Guid.TryParse(Value, out var id);
     public override string ToString() => Value;
+    public override bool Equals(object? obj)
+    {
+        if (obj is EntityId entityId) 
+            return entityId.Value == Value;
+
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public static bool operator ==(EntityId left, EntityId right)
+        => left is null || right is null ? false : left.Equals(right);
+
+    public static bool operator !=(EntityId left, EntityId right)
+       => left is null || right is null ? true : !left.Equals(right);
 }
