f9b4b7c6efa5f5a484c7be95b71d974171c5504d
drlsn <dariusz.lusnia@gmail.com>
2023-09-20 15:30:14 +0200

    Add non-generic IRepository interface

--- Corelibs.Basic/Corelibs.Basic/Repository/IRepository.cs
+++ Corelibs.Basic/Corelibs.Basic/Repository/IRepository.cs
@@ -1,23 +1,24 @@
 ﻿using Corelibs.Basic.Blocks;
 
-namespace Corelibs.Basic.Repository
-{
-    public interface IRepository<TEntity, TEntityId>
-    {
-        /// <summary>
-        /// Get resource by identification string.
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>Success result with given type of resource. If none exists then result is also a success, but with no resource inside. If unexpected failure, then false.</returns>
-        Task<Result<TEntity>> GetBy(TEntityId id);
-        Task<Result<TEntity[]>> GetBy(IList<TEntityId> ids);
-        Task<Result<TEntity>> GetOfName(string name, Func<TEntity, string> getName);
-        Task<Result<TEntity[]>> GetAll();
-        Task<Result<TEntity[]>> GetAll(Action<int> setProgress, CancellationToken ct);
-        Task<Result> Save(TEntity item);
-        Task<Result> Clear();
-        Task<Result> Delete(TEntityId id);
+namespace Corelibs.Basic.Repository;
 
-        Task<Result<bool>> ExistsOfName(string name, Func<TEntity, string> getName);
-    }
+public interface IRepository<TEntity, TEntityId> : IRepository
+{
+    /// <summary>
+    /// Get resource by identification string.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Success result with given type of resource. If none exists then result is also a success, but with no resource inside. If unexpected failure, then false.</returns>
+    Task<Result<TEntity>> GetBy(TEntityId id);
+    Task<Result<TEntity[]>> GetBy(IList<TEntityId> ids);
+    Task<Result<TEntity>> GetOfName(string name, Func<TEntity, string> getName);
+    Task<Result<TEntity[]>> GetAll();
+    Task<Result<TEntity[]>> GetAll(Action<int> setProgress, CancellationToken ct);
+    Task<Result> Save(TEntity item);
+    Task<Result> Clear();
+    Task<Result> Delete(TEntityId id);
+
+    Task<Result<bool>> ExistsOfName(string name, Func<TEntity, string> getName);
 }
+
+public interface IRepository {}
