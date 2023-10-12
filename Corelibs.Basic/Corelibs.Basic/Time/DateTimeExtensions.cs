26a0d99020f638ab7a793b6eb5452ea900270b45
drlsn <dariusz.lusnia@gmail.com>
2023-10-12 18:40:23 +0200

    Add date time extension to get morning

---
+++ Corelibs.Basic/Corelibs.Basic/Time/DateTimeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Corelibs.Basic.Time;
+
+public static class DateTimeExtensions
+{
+    public static DateTime ToMorning(this DateTime date, int morningHour = 4) =>
+        date.AddHours(morningHour - date.Hour);
+}
