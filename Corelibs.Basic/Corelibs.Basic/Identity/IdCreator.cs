7e23370df6acad88d72381b3a378040895e04c20
drlsn <dariusz.lusnia@gmail.com>
2023-11-05 19:02:25 +0100

    Add function to create base 64 id from guid

---
+++ Corelibs.Basic/Corelibs.Basic/Identity/IdCreator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Corelibs.Basic.Identity;
+
+public static class IdCreator
+{
+    public static string CreateBase64GuidId() =>
+        Regex.Replace(
+            Convert.ToBase64String(
+                Guid.NewGuid().ToByteArray()),
+            "[^a-zA-Z0-9]",
+            "");
+}
